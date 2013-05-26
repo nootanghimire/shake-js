cd36af6e40fc8c09a30c2d8325cf6880fb8bafe2
nootanghimire <nootan.ghimire@gmail.com>
2013-05-26 22:18:38 +0545

    Wrapped functions to the object

--- shakeJS.js
+++ shakeJS.js
@@ -1,20 +1,30 @@
 //Initialize Variables
 
-var Shake_SetIntrvl;
-var Shake_count = 1;	
-var Shake_Progress = 0;
-var Shake_left;
-var Max_Shake;
 
-//a helper function
-var setLeft = function(width, divObj) {
+//Todo: Wrap up shake inside the $$ object. 
+
+//The main object : $$
+var $$ = {
+	Shake_SetIntrvl:,
+	Shake_count:1,
+	Shake_Progress:0,
+	Shake_left:,
+	Max_Shake:,
+	
+	AlignCenter: function(divObject) {
+		var totalwidth = document.body.offsetWidth;	
+		var obj = divObject.style.width;
+		var objwidth = obj.replace("px","");
+		var left = eval((eval(totalwidth)/2) - (eval(objwidth)/2));
+		var leftstr = left.toString() + "px";
+		divObject.style.left = leftstr;
+	},
+	setLeft: function(width, divObj) {
 		divObj.style.left = width; 
 	
-	};
+	},
 
-
-//The shake function (core part)
-var shake = function(currentWidth ,ShakeDistance, shakeObj){
+	shakeCore: function(currentWidth ,ShakeDistance, shakeObj){
 		shakeObj = document.getElementById(shakeObj);
 		Shake_Progress = 1;
 		currentLeft=currentWidth.replace("px","");
@@ -26,25 +36,10 @@ var shake = function(currentWidth ,ShakeDistance, shakeObj){
 			clearInterval(Shake_SetIntrvl);
 			Shake_count = 1;
 			Shake_Progress = 0;
-			setLeft(Shake_left, shakeObj);
+			this.setLeft(Shake_left, shakeObj);
 		}
 		
-	};
-//Todo: Wrap up shake inside the $$ object. And make it private
-
-//The main object : $$
-var $$ = {
-	
-	
-	AlignCenter: function(divObject) {
-		var totalwidth = document.body.offsetWidth;	
-		var obj = divObject.style.width;
-		var objwidth = obj.replace("px","");
-		var left = eval((eval(totalwidth)/2) - (eval(objwidth)/2));
-		var leftstr = left.toString() + "px";
-		divObject.style.left = leftstr;
-	},	
-	
+	},
 	shake: function(shakedivName, shakeDistance, shakeDelay, shakeTotal) {
 			if(shakeDistance == NaN || ShakeDistance == undefined) {
 				shakeDistance = 10;
@@ -58,7 +53,7 @@ var $$ = {
 		shakeObj = document.getElementById(shakedivName);
 		Max_Shake = shakeTotal;
 		function make_shake(currentWidth, ShakeDistance, ShakeObj) {
-			var str_wala = "shake( \""+currentWidth+"\", "+ShakeDistance+",\""+ShakeObj+"\")";
+			var str_wala = "this.shakeCore( \""+currentWidth+"\", "+ShakeDistance+",\""+ShakeObj+"\")";
 			Shake_SetIntrvl = setInterval(str_wala,shakeDelay);	
 		}
 		
